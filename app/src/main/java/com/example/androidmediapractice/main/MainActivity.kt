84683ea240e78b5357df0a9bb5a88a2cbedfb153
Wafer <omyshokami@gmail.com>
2019-08-05 13:59:34 +0800

    [Task1] SurfaceView

--- app/src/main/java/com/example/androidmediapractice/main/MainActivity.kt
+++ app/src/main/java/com/example/androidmediapractice/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.androidmediapractice.main
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmediapractice.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,8 +20,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun initRecyclerView() {
         val taskActivities = resources.getStringArray(R.array.task_activities)
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         recyclerView.adapter = MainAdapter(resources.getStringArray(R.array.tasks).toList()) {
-            startActivity(Intent(this, Class.forName(taskActivities[it])))
+            startActivity(Intent(this, Class.forName("$packageName.main.${taskActivities[it]}")))
         }
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL))
     }
--- app/src/main/java/com/example/androidmediapractice/main/task1/CustomSurfaceView.kt
+++ app/src/main/java/com/example/androidmediapractice/main/task1/CustomSurfaceView.kt
@@ -1,19 +1,39 @@
 package com.example.androidmediapractice.main.task1
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import androidx.core.graphics.drawable.toBitmap
+import com.example.androidmediapractice.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 
-class CustomSurfaceView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : SurfaceView(context, attrs, defStyleAttr), SurfaceHolder.Callback {
+class CustomSurfaceView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
+    SurfaceView(context, attrs, defStyleAttr), SurfaceHolder.Callback {
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context?) : this(context, null)
 
-    private var isDrawing = false
+    private var widthMeasureSpec: Int = 0
+    private var heightMeasureSpec: Int = 0
 
     init {
         holder.addCallback(this)
+        keepScreenOn = true
+        isFocusableInTouchMode = true
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        if (this.widthMeasureSpec == 0)
+            this.widthMeasureSpec = widthMeasureSpec
+        if (this.heightMeasureSpec == 0)
+            this.heightMeasureSpec = heightMeasureSpec
+        super.onMeasure(this.widthMeasureSpec, this.heightMeasureSpec)
     }
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
@@ -23,8 +43,22 @@ class CustomSurfaceView(context: Context?, attrs: AttributeSet?, defStyleAttr: I
     }
 
     override fun surfaceCreated(holder: SurfaceHolder?) {
-        isDrawing = true
         thread(start = true) {
+            if (holder != null) {
+                draw(holder)
+            }
         }
+    }
+
+    private fun draw(holder: SurfaceHolder) {
+        val bitmap = context.getDrawable(R.mipmap.ic_launcher).toBitmap()
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(bitmap.height, MeasureSpec.AT_MOST)
+        GlobalScope.launch(Dispatchers.Main) {
+            requestLayout()
+        }
+        val canvas = holder.lockCanvas(Rect(0, 0, this.width, this.height))
+        canvas.drawColor(Color.WHITE)
+        canvas.drawBitmap(bitmap, ((this.width - bitmap.width) / 2).toFloat(), 0F, Paint())
+        holder.unlockCanvasAndPost(canvas)
     }
 }
