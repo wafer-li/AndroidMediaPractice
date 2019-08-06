cb395e5591e40532c27191a34f2c203e2ab14216
Wafer <omyshokami@gmail.com>
2019-08-06 10:25:06 +0800

    [Task2] Setup

--- app/src/main/java/com/example/androidmediapractice/main/task2/Task2Activity.kt
+++ app/src/main/java/com/example/androidmediapractice/main/task2/Task2Activity.kt
@@ -1,5 +1,12 @@
 package com.example.androidmediapractice.main.task2
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidmediapractice.R
 
-class Task2Activity : AppCompatActivity()
+class Task2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task2)
+    }
+}
