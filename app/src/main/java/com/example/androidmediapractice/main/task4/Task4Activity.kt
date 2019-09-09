8d3f22c47c44600566f04d7ed6300fb26b98c423
Wafer <omyshokami@gmail.com>
2019-09-09 17:19:50 +0800

    Setup task4 skeleton

---
+++ app/src/main/java/com/example/androidmediapractice/main/task4/Task4Activity.kt
@@ -0,0 +1,12 @@
+package com.example.androidmediapractice.main.task4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidmediapractice.R
+
+class Task4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task4)
+    }
+}
