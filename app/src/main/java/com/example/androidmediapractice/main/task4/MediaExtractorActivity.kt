429f5bbc78ea45ac387d2cb37ec87937c3899117
Wafer <omyshokami@gmail.com>
2019-09-12 15:08:34 +0800

    Add MediaExtractor

--- app/src/main/java/com/example/androidmediapractice/main/task4/MediaExtractorActivity.kt
+++ app/src/main/java/com/example/androidmediapractice/main/task4/MediaExtractorActivity.kt
@@ -9,6 +9,9 @@ import android.view.SurfaceHolder
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidmediapractice.R
 import kotlinx.android.synthetic.main.activity_media_extractor.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.RuntimePermissions
 
@@ -17,10 +20,12 @@ class MediaExtractorActivity : AppCompatActivity() {
 
     private lateinit var mediaExtractor: MediaExtractor
     private lateinit var mediaFormat: MediaFormat
-    private lateinit var codec: MediaCodec
+    private lateinit var mediaDecoder: MediaCodec
 
     private var width = 0
     private var height = 0
+
+    private var isPlaying = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,16 +44,24 @@ class MediaExtractorActivity : AppCompatActivity() {
         })
     }
 
+    override fun onPause() {
+        super.onPause()
+        stopPlay()
+    }
+
     @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
     internal fun startPlay() {
-        initMediaExtractor()
-        initVideoSpecs(mediaFormat)
-        initCodec(mediaFormat)
+        GlobalScope.launch(Dispatchers.IO) {
+            initMediaExtractor()
+            initVideoSpecs(mediaFormat)
+            initCodec(mediaFormat)
+            play()
+        }
     }
 
     private fun initMediaExtractor() {
         mediaExtractor = MediaExtractor()
-        mediaExtractor.setDataSource("sample.mp4")
+        mediaExtractor.setDataSource(resources.assets.openFd("sample.mp4"))
         val trackIndex = mediaExtractor.findTrackIndex {
             val mime = it.getString(MediaFormat.KEY_MIME)
             mime?.startsWith("video/") ?: false
@@ -64,12 +77,60 @@ class MediaExtractorActivity : AppCompatActivity() {
 
     private fun initCodec(mediaFormat: MediaFormat) {
         val mime = mediaFormat.getString(MediaFormat.KEY_MIME) ?: error("MIME Empty")
-        codec = MediaCodec.createDecoderByType(mime)
-        codec.configure(mediaFormat, mediaPlaySurfaceView.holder.surface, null, 0)
-        codec.start()
+        mediaDecoder = MediaCodec.createDecoderByType(mime)
+        mediaDecoder.configure(mediaFormat, mediaPlaySurfaceView.holder.surface, null, 0)
     }
 
     private fun play() {
+        isPlaying = true
+        mediaDecoder.start()
+        do {
+            val isEos =
+                mediaExtractor.sampleFlags and MediaCodec.BUFFER_FLAG_END_OF_STREAM == MediaCodec.BUFFER_FLAG_END_OF_STREAM
+            if (!isEos) {
+                val inputBufferIndex = mediaDecoder.dequeueInputBuffer(0L)
+                if (inputBufferIndex >= 0) {
+                    val buffer = mediaDecoder.getInputBuffer(inputBufferIndex)
+                    val size = if (buffer != null) {
+                        mediaExtractor.readSampleData(buffer, 0)
+                    } else -1
+                    val flag =
+                        mediaExtractor.sampleFlags.let { if (size <= 0) it and MediaCodec.BUFFER_FLAG_END_OF_STREAM else it }
+
+                    mediaDecoder.queueInputBuffer(
+                        inputBufferIndex,
+                        0,
+                        size,
+                        mediaExtractor.sampleTime,
+                        flag
+                    )
+
+                    if (buffer != null) {
+                        mediaExtractor.advance()
+                    }
+                }
+            }
+
+            val outputInfo = MediaCodec.BufferInfo()
+            val outputBufferIndex = mediaDecoder.dequeueOutputBuffer(outputInfo, 0L)
+
+            if (outputInfo.size <= 0 && isEos) {
+                stopPlay()
+                break
+            } else if (outputBufferIndex >= 0) {
+                mediaDecoder.releaseOutputBuffer(outputBufferIndex, true)
+            }
+
+        } while (true)
+    }
+
+    private fun stopPlay() {
+        if (isPlaying) {
+            isPlaying = false
+            mediaDecoder.stop()
+            mediaDecoder.release()
+            mediaExtractor.release()
+        }
     }
 
     private fun MediaExtractor.findTrackIndex(block: (MediaFormat) -> Boolean): Int {
--- app/src/main/java/com/example/androidmediapractice/main/task4/Task4Activity.kt
+++ app/src/main/java/com/example/androidmediapractice/main/task4/Task4Activity.kt
@@ -1,12 +1,17 @@
 package com.example.androidmediapractice.main.task4
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidmediapractice.R
+import kotlinx.android.synthetic.main.activity_task4.*
 
 class Task4Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task4)
+        mediaExtractorBtn.setOnClickListener {
+            startActivity(Intent(this, MediaExtractorActivity::class.java))
+        }
     }
 }
