759299db31260843eee9973e0b516f5e0e65effb
Wafer Li <omyshokami@gmail.com>
2019-09-16 20:01:33 +0800

    Add GLSurfaceView

--- app/src/main/java/com/example/androidmediapractice/main/task5/Task5Activity.kt
+++ app/src/main/java/com/example/androidmediapractice/main/task5/Task5Activity.kt
@@ -1,5 +1,52 @@
 package com.example.androidmediapractice.main.task5
 
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
-class Task5Activity : AppCompatActivity()
+class Task5Activity : AppCompatActivity() {
+    private lateinit var glSurfaceView: GLSurfaceView
+    private val renderer = object : GLSurfaceView.Renderer {
+        override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
+            // Set the background frame color
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        }
+
+        override fun onDrawFrame(unused: GL10) {
+            // Redraw background color
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        }
+
+        override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
+            GLES20.glViewport(0, 0, width, height)
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        glSurfaceView = GLSurfaceView(this)
+        setContentView(glSurfaceView)
+        initGlSurfaceView(glSurfaceView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glSurfaceView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        glSurfaceView.onPause()
+    }
+
+    private fun initGlSurfaceView(glSurfaceView: GLSurfaceView) {
+        glSurfaceView.run {
+            setEGLContextClientVersion(2)
+            setRenderer(renderer)
+        }
+    }
+}
