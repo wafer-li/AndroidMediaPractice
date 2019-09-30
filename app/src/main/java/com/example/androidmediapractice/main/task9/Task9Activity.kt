4c965bcdb2240efd5c6a8fb131b98a50957668a7
Wafer <omyshokami@gmail.com>
2019-09-30 12:00:06 +0800

    Add task9 skeleton

---
+++ app/src/main/java/com/example/androidmediapractice/main/task9/Task9Activity.kt
@@ -0,0 +1,12 @@
+package com.example.androidmediapractice.main.task9
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidmediapractice.R
+
+class Task9Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task9)
+    }
+}
